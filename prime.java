9a7a650dad9dfdd6d504110541103e97f7324819
vijayaingale1999 <113347112+vijayaingale1999@users.noreply.github.com>
2022-09-18 16:42:33 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class prime {
+    //to find prime no
+    
+    public static void main(String[] args)
+    {
+       Scanner s= new Scanner(System.in);
+       int i=s.nextInt();
+       {
+           if(isPrime(i))
+           {
+               System.out.println("the no is prime");
+           }
+       }
+
+    }
+
+    private static boolean isPrime(int i) {
+        return false;
+    }
+}
