24ff831a41868d5f3937a24ba102466474502d69
vijayaingale1999 <113347112+vijayaingale1999@users.noreply.github.com>
2022-09-18 16:59:16 +0530

    Add files via upload

---
+++ amstrong.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class amstrong {
+    //amstrong no is the positive m digit no that is equal to the sum of mth power of their digit
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int i= s.nextInt();
+        int j=i,count=0;
+         while(j==0)
+         {
+          count++;
+           j=j/10;
+         }
+        
+    }
+    
+}
