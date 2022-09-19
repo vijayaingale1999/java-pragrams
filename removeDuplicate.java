6fe3929ce71cadef8453142f28a42279aad5eb98
vijayaingale1999 <113347112+vijayaingale1999@users.noreply.github.com>
2022-09-19 19:12:35 +0530

    Add files via upload

---
+++ removeDuplicate.java
@@ -0,0 +1,8 @@
+public class removeDuplicate {
+    //java program to reove duplicate element from an array
+    public static void main(String[] args)
+    {
+        int[] i={10,30,50,40,10,30};
+        
+    }
+}
